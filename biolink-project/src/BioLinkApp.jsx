925a35da195d2aed41b991c6026c8ef069786a81
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2023-06-17 16:52:56 -0700

    main app

---
+++ biolink-project/src/BioLinkApp.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BioLink from './components/BioLink';
+
+function BioLinkApp() {
+  return (
+    <div className="App">
+      <BioLink />
+    </div>
+  );
+}
+
+export default BioLinkApp;
