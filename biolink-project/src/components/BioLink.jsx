4d007bf81d2dba77ebda065e6e227fbfa7537e88
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2023-06-17 18:39:17 -0700

    problem fixes and content

--- biolink-project/src/components/BioLink.jsx
+++ biolink-project/src/components/BioLink.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from './NavBar';
 import Home from './Home';
+import Features from './Features';
+import ProblemStatement from './ProblemStatement';
 
 function BioLink() {
     return (
         <div className="BioLink">
             <NavBar />
             <Home />
+            <Features />
+            <ProblemStatement />
         </div>
     );
 }
